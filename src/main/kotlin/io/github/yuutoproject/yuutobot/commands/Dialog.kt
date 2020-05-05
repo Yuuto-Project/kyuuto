2cdf099f8c097b4aac87a48a7955fbe86dbfae1f
not-archiey <notarchiey@protonmail.com>
2020-05-05 13:40:57 +0700

    Lint Fix

--- src/main/kotlin/io/github/yuutoproject/yuutobot/commands/Dialog.kt
+++ src/main/kotlin/io/github/yuutoproject/yuutobot/commands/Dialog.kt
@@ -35,7 +35,7 @@ import org.slf4j.LoggerFactory
 class Dialog : AbstractCommand("dialog", CommandCategory.INFO, "does something", "[bg] <char> <text>") {
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
-    private val backgrounds : List<String> = listOf(
+    private val backgrounds: List<String> = listOf(
         "bath",
         "beach",
         "cabin",
@@ -45,7 +45,7 @@ class Dialog : AbstractCommand("dialog", CommandCategory.INFO, "does something",
         "messhall"
     )
 
-    private val characters : List<String> = listOf(
+    private val characters: List<String> = listOf(
         "aiden",
         "avan",
         "chiaki",
@@ -70,49 +70,49 @@ class Dialog : AbstractCommand("dialog", CommandCategory.INFO, "does something",
         "yuuto"
     )
 
-    private val backgroundsString : String = backgrounds.joinToString("`, `")
-    private val charactersString : String = characters  .joinToString("`, `")
+    private val backgroundsString: String = backgrounds.joinToString("`, `")
+    private val charactersString: String = characters.joinToString("`, `")
 
-    //Using ExperimentalStdLib for .removeFirst() in Mutable Lists
+    // Using ExperimentalStdLib for .removeFirst() in Mutable Lists
     @ExperimentalStdlibApi
     override fun run(args: MutableList<String>, event: GuildMessageReceivedEvent) {
         val moveThisToAStorageLocation = OkHttpClient()
 
-        val now : Long = System.currentTimeMillis()
+        val now: Long = System.currentTimeMillis()
 
-        if(args.count() < 2){
+        if (args.count() < 2) {
             event.channel.sendMessage("This command requires two arguments : `dialog [background] <character> <text>` ([] is optional)").queue()
             return
         }
 
-        var character : String = args.removeFirst().toLowerCase()
-        val background : String
+        var character: String = args.removeFirst().toLowerCase()
+        val background: String
 
-        if(characters.contains(character)){
+        if (characters.contains(character)) {
             background = "camp"
         } else {
             background = character
             character = args.removeFirst().toLowerCase()
         }
 
-        if(!backgrounds.contains(background)){
+        if (!backgrounds.contains(background)) {
             event.channel.sendMessage("Sorry but I couldn't find $background as a location\nAvailable backgrounds are : `$backgroundsString`").queue()
             return
         }
 
-        if(!characters.contains(character)){
+        if (!characters.contains(character)) {
             event.channel.sendMessage("Sorry, but I don't think that $character is a character in Camp Buddy\nAvailable characters are : `$charactersString`").queue()
             return
         }
 
-        if(args.count() < 1){
+        if (args.count() < 1) {
             event.channel.sendMessage("Please provide a message to be written~!").queue()
             return
         }
 
-        val text : String = args.joinToString(" ").replace("/[‘’]/g".toRegex(), "'")
+        val text: String = args.joinToString(" ").replace("/[‘’]/g".toRegex(), "'")
 
-        if(
+        if (
             EMOTE_MENTIONS_REGEX.containsMatchIn(text) ||
             EMOJI_REGEX.containsMatchIn(text) ||
             NONASCII_REGEX.containsMatchIn(text)
@@ -141,7 +141,7 @@ class Dialog : AbstractCommand("dialog", CommandCategory.INFO, "does something",
             }
 
             override fun onResponse(call: Call, response: Response) {
-                if(response.code != 200){
+                if (response.code != 200) {
                     event.channel.sendMessage("An error just happened in me, blame the devs <:YoichiLol:701312070880329800>").queue()
                     return
                 }
@@ -153,7 +153,7 @@ class Dialog : AbstractCommand("dialog", CommandCategory.INFO, "does something",
                     .append(", Here ya go!")
                     .queue()
 
-                logger.info("Generated image for $character at $background, took ${System.currentTimeMillis()-now}ms")
+                logger.info("Generated image for $character at $background, took ${System.currentTimeMillis() - now}ms")
             }
         })
     }
