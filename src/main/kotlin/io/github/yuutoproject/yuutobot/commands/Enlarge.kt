53989522f18ef4891bdef9eb03284f83d2849f4a
not-archiey <notarchiey@protonmail.com>
2020-05-05 14:43:42 +0700

    Added enlarge command

---
+++ src/main/kotlin/io/github/yuutoproject/yuutobot/commands/Enlarge.kt
@@ -0,0 +1,43 @@
+/*
+ * Open source bot built by and for the Camp Buddy Discord Fan Server.
+ *     Copyright (C) 2020  Kyuuto-devs
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.github.yuutoproject.yuutobot.commands
+
+import io.github.yuutoproject.yuutobot.commands.base.AbstractCommand
+import io.github.yuutoproject.yuutobot.commands.base.CommandCategory
+import io.github.yuutoproject.yuutobot.utils.EMOTE_ID_REGEX
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+
+class Enlarge : AbstractCommand("enlarge", CommandCategory.UTIL, "Returns an enlarged emote", "Run `enlarge <emote>` to get the full link to `<emote>` at a large size") {
+    override fun run(args: MutableList<String>, event: GuildMessageReceivedEvent) {
+        if(args.count() == 0){
+            event.channel.sendMessage("Sorry, but you need to provide me an emote to use this command~!").queue()
+            return;
+        }
+
+        if(!EMOTE_ID_REGEX.containsMatchIn(args[0])){
+            event.channel.sendMessage("Sorry, but to use this command you need to send emotes only~!").queue()
+            return;
+        }
+
+        val emoteId = EMOTE_ID_REGEX.find(args[0])?.value;
+
+        println(emoteId);
+    }
+
+}
